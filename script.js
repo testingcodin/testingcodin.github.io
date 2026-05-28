84f1518da74a8eb1418362ac9a839f7e6e0961a8
testingcodin <teo1202005@gmail.com>
2026-05-27 19:34:25 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var task;
+function addtask() {
+const newtask = document.getElementById('task');
+alert('hi'); 
+};
+const add = document.getElementById('add');
+add.addEventListener('click', addtask() ) ;
