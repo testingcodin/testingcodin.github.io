3f21bdce21a7a2598540cfc6882c79a098f0efa8
testingcodin <teo1202005@gmail.com>
2026-05-28 00:40:23 -0500

    This land is your land - JibJab

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-var tasks = [];
 function addtask() {
+    if (localStorage.getItem('todolist') == null) {
+        var newtask = document.getElementById('task').value;
+        localStorage.setItem('todolist', JSON.stringify([newtask]));
+    } else {
+    
+    const list = getarray();
+    console.log(getarray() + 'addtask')
+    
     var newtask = document.getElementById('task').value;
-    if (localStorage.todolist == null){
-        tasks = ["short"];
-        tasks.length = 0;
+    console.log(JSON.stringify(newtask) + 'jsonstringify')
+    list[list.length] = newtask
+    localStorage.setItem('todolist', JSON.stringify(list));   
     }
-    if (tasks.length > 0) {
-       tasks[tasks.length] = newtask;
-    }   else {
-            tasks = [newtask];
-        }
-    localStorage.setItem('todolist', JSON.stringify(tasks));    
 };
+function getarray() {
+    var array = localStorage.getItem('todolist');
+    const retrn = JSON.parse(array)
+    console.log(JSON.parse(array) + "getarrayjson")
+    console.log(retrn + 'getarrayretrn')
+    return retrn;  
+}
+function removetask() {
+
+}
+function cleararray() {
+    var list = ["short"];
+    list.length = 0;
+    localStorage.setItem('todolist', JSON.stringify(list));    
+}
 const add = document.getElementById('add');
 add.addEventListener('click', addtask);
+const clear = document.getElementById('clear');
+clear.addEventListener('click', cleararray);
+
+
+
