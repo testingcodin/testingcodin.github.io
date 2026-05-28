f0c103ef5acc301e1c3e028846ac272159c372be
testingcodin <teo1202005@gmail.com>
2026-05-27 20:14:47 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var task;
 function addtask() {
     const newtask = document.getElementById('task')
-    alert('hi')
+    const tasks =[];
+    if (tasks.length > 0) {
+       tasks[tasks.length] = newtask;
+    }
+    else {
+        tasks = [newtask];
+    }
+    localStorage.setItem('todolist', JSON.stringify(tasks))    
 };
 const add = document.getElementById('add');
 add.addEventListener('click', addtask) ;
